e5054b660846a6844330888b5efc31778ec67a89
voidptrr <bruno.tommaso@protonmail.com>
2026-07-04 23:42:22 +0100

    chore: add clang-format

--- hands-on-network-programming-with-c/chapter2.c
+++ hands-on-network-programming-with-c/chapter2.c
@@ -9,93 +9,91 @@
 #include <unistd.h>
 #include <errno.h>
 
-int main() {
-    printf("Configuring local address...\n");
-    struct addrinfo hints;
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_INET6;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE;
+int main()
+{
+	printf("Configuring local address...\n");
+	struct addrinfo hints;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_family = AF_INET6;
+	hints.ai_socktype = SOCK_STREAM;
+	hints.ai_flags = AI_PASSIVE;
 
-    struct addrinfo *bind_address;
-    int code = getaddrinfo(NULL, "8080", &hints, &bind_address);
-    if (code != 0) {
-        fprintf(stderr, "%s", gai_strerror(code));
-        return 1;
-    }
+	struct addrinfo *bind_address;
+	int code = getaddrinfo(NULL, "8080", &hints, &bind_address);
+	if (code != 0) {
+		fprintf(stderr, "%s", gai_strerror(code));
+		return 1;
+	}
 
-    int socket_listen = socket(
-            bind_address->ai_family, 
-            bind_address->ai_socktype, 
-            bind_address->ai_protocol
-    );
+	int socket_listen =
+		socket(bind_address->ai_family, bind_address->ai_socktype,
+		       bind_address->ai_protocol);
 
-    if (socket_listen < 0) {
-        fprintf(stderr, "socket() failed. (%d)\n", errno);
-        return 1;
-    }
+	if (socket_listen < 0) {
+		fprintf(stderr, "socket() failed. (%d)\n", errno);
+		return 1;
+	}
 
-    int option = 0;
-    if (setsockopt(socket_listen, IPPROTO_IPV6, IPV6_V6ONLY,
-                (void*)&option, sizeof(option))) {
-        fprintf(stderr, "setsockopt() failed. (%d)\n", errno);
-        return 1;
-    }
+	int option = 0;
+	if (setsockopt(socket_listen, IPPROTO_IPV6, IPV6_V6ONLY,
+		       (void *)&option, sizeof(option))) {
+		fprintf(stderr, "setsockopt() failed. (%d)\n", errno);
+		return 1;
+	}
 
-    if (bind(socket_listen,
-                bind_address->ai_addr, bind_address->ai_addrlen)) {
-        fprintf(stderr, "bind() failed. (%d) \n", errno);
-        return 1;
-    }
+	if (bind(socket_listen, bind_address->ai_addr,
+		 bind_address->ai_addrlen)) {
+		fprintf(stderr, "bind() failed. (%d) \n", errno);
+		return 1;
+	}
 
-    freeaddrinfo(bind_address);
+	freeaddrinfo(bind_address);
 
-    printf("Listening...\n");
-    if (listen(socket_listen, 10) < 0) {
-        fprintf(stderr, "listen() failed. (%d)\n", errno);
-        return 1;
-    }
+	printf("Listening...\n");
+	if (listen(socket_listen, 10) < 0) {
+		fprintf(stderr, "listen() failed. (%d)\n", errno);
+		return 1;
+	}
 
-    printf("Waiting for connection...\n");
-    struct sockaddr_storage client_address;
-    socklen_t client_len = sizeof(client_address);
-    int socket_client = accept(socket_listen,
-            (struct sockaddr*) &client_address, &client_len);
+	printf("Waiting for connection...\n");
+	struct sockaddr_storage client_address;
+	socklen_t client_len = sizeof(client_address);
+	int socket_client = accept(
+		socket_listen, (struct sockaddr *)&client_address, &client_len);
 
-    if (socket_client < 0) {
-        fprintf(stderr, "accept() failed. (%d)\n", errno);
-        return 1;
-    }
+	if (socket_client < 0) {
+		fprintf(stderr, "accept() failed. (%d)\n", errno);
+		return 1;
+	}
 
-    printf("Client is connected...");
-    char address_buffer[100];
-    getnameinfo((struct sockaddr*)&client_address,
-            client_len, address_buffer, sizeof(address_buffer), 0, 0,
-            NI_NUMERICHOST);
-    printf("%s\n", address_buffer);
+	printf("Client is connected...");
+	char address_buffer[100];
+	getnameinfo((struct sockaddr *)&client_address, client_len,
+		    address_buffer, sizeof(address_buffer), 0, 0,
+		    NI_NUMERICHOST);
+	printf("%s\n", address_buffer);
 
-    printf("Reading request...\n");
-    char request[1024];
-    int bytes_received = recv(socket_client, request, 1024, 0);
-    printf("Received %d bytes.\n", bytes_received);
+	printf("Reading request...\n");
+	char request[1024];
+	int bytes_received = recv(socket_client, request, 1024, 0);
+	printf("Received %d bytes.\n", bytes_received);
 
-    printf("sending response...\n");
-    const char *response = 
-        "HTTP/1.1 200 OK\r\n"
-        "Connection: close\r\n"
-        "Content-Type: text/plain\r\n\r\n"
-        "Local time is: ";
-    int bytes_sent = send(socket_client, response, strlen(response), 0);
-    printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(response));
+	printf("sending response...\n");
+	const char *response = "HTTP/1.1 200 OK\r\n"
+			       "Connection: close\r\n"
+			       "Content-Type: text/plain\r\n\r\n"
+			       "Local time is: ";
+	int bytes_sent = send(socket_client, response, strlen(response), 0);
+	printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(response));
 
-    time_t timer;
-    time(&timer);
-    char *time_msg = ctime(&timer);
-    bytes_sent = send(socket_client, time_msg, strlen(time_msg), 0);
-    printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(time_msg));
+	time_t timer;
+	time(&timer);
+	char *time_msg = ctime(&timer);
+	bytes_sent = send(socket_client, time_msg, strlen(time_msg), 0);
+	printf("Sent %d of %d bytes.\n", bytes_sent, (int)strlen(time_msg));
 
-    close(socket_client);
-    close(socket_listen);
+	close(socket_client);
+	close(socket_listen);
 
-    return 0;
+	return 0;
 }
--- hands-on-network-programming-with-c/chapter3/tcp_client.c
+++ hands-on-network-programming-with-c/chapter3/tcp_client.c
@@ -9,91 +9,94 @@
 #include <unistd.h>
 #include <errno.h>
 
-int main(int argc, char** argv) {
-    if (argc < 3) {
-        fprintf(stderr, "usage: ./tcp_client hostname port\n");
-        return 1;
-    }
+int main(int argc, char **argv)
+{
+	if (argc < 3) {
+		fprintf(stderr, "usage: ./tcp_client hostname port\n");
+		return 1;
+	}
 
-    printf("Configuring remote address...\n");
-    struct addrinfo hints;
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_socktype = SOCK_STREAM;
+	printf("Configuring remote address...\n");
+	struct addrinfo hints;
+	memset(&hints, 0, sizeof(hints));
+	hints.ai_socktype = SOCK_STREAM;
 
-    struct addrinfo *peer_address;
-    int code = getaddrinfo(argv[1], argv[2], &hints, &peer_address);
-    if (code != 0) {
-        fprintf(stderr, "%s", gai_strerror(code));
-        return 1;
-    }
+	struct addrinfo *peer_address;
+	int code = getaddrinfo(argv[1], argv[2], &hints, &peer_address);
+	if (code != 0) {
+		fprintf(stderr, "%s", gai_strerror(code));
+		return 1;
+	}
 
-    printf("Remote address is: ");
-    char address_buffer[100];
-    char service_buffer[100];
-    getnameinfo(peer_address->ai_addr, peer_address->ai_addrlen,
-            address_buffer, sizeof(address_buffer),
-            service_buffer, sizeof(service_buffer),
-            NI_NUMERICHOST);
-    printf("%s %s\n", address_buffer, service_buffer);
+	printf("Remote address is: ");
+	char address_buffer[100];
+	char service_buffer[100];
+	getnameinfo(peer_address->ai_addr, peer_address->ai_addrlen,
+		    address_buffer, sizeof(address_buffer), service_buffer,
+		    sizeof(service_buffer), NI_NUMERICHOST);
+	printf("%s %s\n", address_buffer, service_buffer);
 
-    printf("Creating socket...\n");
-    int socket_peer = socket(peer_address->ai_family,
-            peer_address->ai_socktype, peer_address->ai_protocol);
-    if (socket_peer < 0) {
-        fprintf(stderr, "socket() failed. (%d)\n", errno);
-        return 1;
-    }
+	printf("Creating socket...\n");
+	int socket_peer =
+		socket(peer_address->ai_family, peer_address->ai_socktype,
+		       peer_address->ai_protocol);
+	if (socket_peer < 0) {
+		fprintf(stderr, "socket() failed. (%d)\n", errno);
+		return 1;
+	}
 
-    printf("Connecting...\n");
-    if (connect(socket_peer,
-                peer_address->ai_addr, peer_address->ai_addrlen)) {
-        fprintf(stderr, "connect() failed. (%d)\n", errno);
-        return 1;
-    }
+	printf("Connecting...\n");
+	if (connect(socket_peer, peer_address->ai_addr,
+		    peer_address->ai_addrlen)) {
+		fprintf(stderr, "connect() failed. (%d)\n", errno);
+		return 1;
+	}
 
-    freeaddrinfo(peer_address);
+	freeaddrinfo(peer_address);
 
-    printf("Connected.\n");
-    printf("To send data, enter text followed by enter.\n");
+	printf("Connected.\n");
+	printf("To send data, enter text followed by enter.\n");
 
-    for (;;) {
-        fd_set reads;
-        FD_ZERO(&reads);
-        FD_SET(socket_peer, &reads);
-        FD_SET(fileno(stdin), &reads);
+	for (;;) {
+		fd_set reads;
+		FD_ZERO(&reads);
+		FD_SET(socket_peer, &reads);
+		FD_SET(fileno(stdin), &reads);
 
-        struct timeval timeout;
-        timeout.tv_sec = 0;
-        timeout.tv_usec = 100000;
+		struct timeval timeout;
+		timeout.tv_sec = 0;
+		timeout.tv_usec = 100000;
 
-        if (select(socket_peer + 1, &reads, 0, 0, &timeout) < 0) {
-            fprintf(stderr, "select() failed. (%d)\n", errno);
-            return 1;
-        }
+		if (select(socket_peer + 1, &reads, 0, 0, &timeout) < 0) {
+			fprintf(stderr, "select() failed. (%d)\n", errno);
+			return 1;
+		}
 
-        if (FD_ISSET(socket_peer, &reads)) {
-            char read[4096];
-            int bytes_received = recv(socket_peer, read, 4096, 0);
-            if (bytes_received < 1) {
-                printf("Connection closed by peer.\n");
-                break;
-            }
+		if (FD_ISSET(socket_peer, &reads)) {
+			char read[4096];
+			int bytes_received = recv(socket_peer, read, 4096, 0);
+			if (bytes_received < 1) {
+				printf("Connection closed by peer.\n");
+				break;
+			}
 
-            printf("Received (%d btes): %.*s",
-                    bytes_received, bytes_received, read);
-        }
+			printf("Received (%d btes): %.*s", bytes_received,
+			       bytes_received, read);
+		}
 
-        if (FD_ISSET(fileno(stdin), &reads)) {
-            char read[4096];
-            if (!fgets(read, 4096, stdin)) break;
-            printf("Sending: %s", read);
-            int bytes_sent = send(socket_peer, read, strlen(read), 0);
-            printf("Sent %d bytes.\n", bytes_sent);
-        }
-    }
+		if (FD_ISSET(fileno(stdin), &reads)) {
+			char read[4096];
+			if (!fgets(read, 4096, stdin))
+				break;
+			printf("Sending: %s", read);
+			int bytes_sent =
+				send(socket_peer, read, strlen(read), 0);
+			printf("Sent %d bytes.\n", bytes_sent);
+		}
+	}
 
-    printf("Closing socket...\n");
-    close(socket_peer);
+	printf("Closing socket...\n");
+	close(socket_peer);
 
-    return 0;
+	return 0;
 }
